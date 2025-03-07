fa10f27045f54b18111f2ab2bbaa871f19aaa74f
SirMauricio <190300383@unicaribe.edu.mx>
2025-03-07 17:43:39 -0500

    Agregar

--- obj/Debug/net8.0/Babel.AssemblyInfo.cs
+++ obj/Debug/net8.0/Babel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Babel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c89ea7137a0a718cff3d15c1a64866550ce83c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b92d7627bff17527913a98f04e52263223359f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Babel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Babel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
